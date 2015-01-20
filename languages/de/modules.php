7fda99804c14e9fd2c74bf2c3fd09665ce1b051e
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2015-01-20 17:43:20 +0100

    Added the German language files (thanks to Didier Federer)

---
+++ languages/de/modules.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * mobile_menu extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2011-2014 Codefog
+ *
+ * @package mobile_menu
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @author  Didier Federer <d.federer@designpilot.ch>
+ * @license LGPL
+ */
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['mobile_menu'] = array('Mobile Menu', 'Fügt ein Mobile Menu auf der Seite ein.');

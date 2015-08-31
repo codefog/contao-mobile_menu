f866eab4f9af04f4c81e62f96e1308d3d2cb7884
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2015-08-31 10:33:18 +0200

    Make sure the menu is not visible upon page load

--- assets/js/mobile-menu.jquery.js
+++ assets/js/mobile-menu.jquery.js
@@ -69,6 +69,9 @@
             });
 
             this.updateElementsVisibility();
+
+            // Once everything is ready make sure the menu is visible
+            this.element.css('display', 'block');
         },
 
         /**
